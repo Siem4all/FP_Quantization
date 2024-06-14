f0f9826cf6fa6258eaae8d5eef32862cb87a9c00
Siem Gebrehiwet <102214627+Siem4all@users.noreply.github.com>
2024-06-14 11:44:06 +0300

    f2ptobits

--- src/webapp.py
+++ src/webapp.py
@@ -69,28 +69,67 @@ def bitsToF2P():
 @app.route('/webapp/F2PtoBits', methods=['POST'])
 def F2PToBits():
     '''
-    This recieves counter size, hyper size, counter bits and flavor type from the form to return the f2p value
-    it represents.
+    This function receives counter size, hyper size, counter bits, and flavor type from the form to return the F2P value it represents.
     '''
     try:
         # Retrieve the form data from the request
-        cntr_size = request.form.get('cntrsize')
-        hyper_size = request.form.get('hypersize')
-        f2p_value = request.form.get('f2pvalue')
-        flavor_type = request.form.get('flavortype')
-        fp_type = request.form.get('fptype')
-        # Call the custom process_form() function
-        cntrValue = float('-inf')
-        # Call the custom process_form() function
-        myCntr = genCntrMasterFxp(cntrSize=int(cntr_size), hyperSize=int(hyper_size), nSystem=str(fp_type), flavor=str(flavor_type), verbose=[])
-        while (f2p_value >= cntrValue):
-            cntrValue = myCntr.incCntr(cntrIdx=0, factor=int(1), mult=False, verbose=[])
-        # Return the result as a JSON response
-        cntr_bits = myCntr.cntrs[0]
-        return jsonify({"result": cntr_bits})
+        cntrSize = int(request.form.get('cntrsize'))
+        hyperSize = int(request.form.get('hypersize'))
+        fp_value = float(request.form.get('fpvalue'))
+        flavor = str(request.form.get('flavortype'))
+        nSystem = str(request.form.get('fptype'))
+        snType = str(request.form.get('sntype'))
+        sn_type = 1 if snType == 'signed' else 0
+
+        expSize = 2 ** hyperSize - 1
+
+        # Validate the input parameters
+        if (sn_type == 0 and (cntrSize < 3 or hyperSize < 1)) or \
+           (sn_type == 1 and (cntrSize < 4 or hyperSize < 1)):
+            return {"result": "Sorry, Either the number of bits or the hyper-exponent size is invalid."}
+
+        if sn_type == 1 and expSize + hyperSize +2 > cntrSize:
+            return {"result": "The hyper-exponent size is too large for the given counter size and sign type."}
+        if sn_type == 0 and expSize + hyperSize +1 > cntrSize:
+            return {"result": "Sorry, The hyper-exponent size is too large for the given counter size and sign type."}
+
+        # Generate the F2P counter and find the closest binary representation
+        myCntr = genCntrMasterFxp(cntrSize=cntrSize-sn_type, hyperSize=hyperSize, nSystem=nSystem, flavor=flavor, verbose=[])
+
+        if sn_type == 1 and fp_value < 0:
+            cntr_bits = None
+            for num in range(2 ** (cntrSize-1)):
+                current_bits = bin(num)[2:].zfill(cntrSize-1)
+                current_fp = -1 * float(myCntr.cntr2num(cntr=str(current_bits)))
+                if fp_value == current_fp:
+                    cntr_bits = f'1{current_bits}'
+                    break
+        elif sn_type == 1 and fp_value >=0:
+            cntr_bits = None
+            for num in range(2 ** (cntrSize-1)):
+                current_bits = bin(num)[2:].zfill(cntrSize-1)
+                current_fp = float(myCntr.cntr2num(cntr=str(current_bits)))
+                if fp_value == current_fp:
+                    cntr_bits = f'0{current_bits}'
+                    break
+        else:
+            cntr_bits = None
+            for num in range(2 ** (cntrSize)):
+                current_bits = bin(num)[2:].zfill(cntrSize)
+                current_fp = myCntr.cntr2num(cntr=str(current_bits))
+                if fp_value == current_fp:
+                    cntr_bits = f'0{current_bits}'
+                    break
+
+        if cntr_bits is None:
+            return {"result": "Sorry, Could not find a suitable binary representation."}
+
+        return {"result": cntr_bits}
+
+    except (ValueError, TypeError):
+        return {"result": "Sorry, Invalid input data."}
     except Exception as e:
-        # Handle any exceptions that occur during the function execution
-        return jsonify({"error": str(e)}), 500
+        return {"result": f"{str(e)}"}
 
 
 if __name__ == '__main__':
