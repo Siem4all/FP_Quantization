66079dd2323e40a74194d4f15b6e91ff872b3dcd
Siem Gebrehiwet <102214627+Siem4all@users.noreply.github.com>
2024-11-30 20:23:47 +0200

    Update Quantizer.py

--- src/Quantizer.py
+++ src/Quantizer.py
@@ -3,16 +3,13 @@ import os, scipy, matplotlib, pickle, numpy as np, seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from fitter import Fitter, get_common_distributions, get_distributions
-
 import settings, ResFileParser, F2P_sr, F2P_lr, F2P_li, FP, SEAD_stat, SEAD_dyn  
 from tictoc import tic, toc
 from printf import printf, printar, printarFp
 from SingleCntrSimulator import main, getAllValsFP, getAllValsFxp
 from ResFileParser import genRndErrFileName, getFxpSettings, colors, colorOfMode, labelOfMode, markerOfMode, MARKER_SIZE_SMALL, FONT_SIZE, FONT_SIZE_SMALL, LEGEND_FONT_SIZE, LEGEND_FONT_SIZE_SMALL
 from settings import error, warning, VERBOSE_RES, VERBOSE_PCL 
-
 MAX_DF = 20
-
 def myFitter (
         vec : np.array,
         ) -> str:
